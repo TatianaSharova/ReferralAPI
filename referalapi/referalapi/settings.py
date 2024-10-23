866b5ad00f9c809fab09411a0810902ac1b6b794
Татьяна Шарова <tanyasharova2001@mail.ru>
2024-10-23 19:03:34 +0400

    add settings for cache

--- referalapi/referalapi/settings.py
+++ referalapi/referalapi/settings.py
@@ -118,6 +118,16 @@ SPECTACULAR_SETTINGS = {
     'SERVE_INCLUDE_SCHEMA': False
 }
 
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+
 LANGUAGE_CODE = 'ru-RU'
 
 TIME_ZONE = 'Europe/Moscow'
